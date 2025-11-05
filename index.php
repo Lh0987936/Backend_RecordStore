5cdde17fa6b3e7422df7c0539cf40aad1ba25f69
HOOVER <lh0987936@dt050563.otc.edu>
2025-11-05 10:17:29 -0600

    close to time, created doesn't work but create does now

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
-<?php 
+<?php
 require 'data/functions.php';
 
-$view = filter_input(INPUT_GET, 'view') ?: 'list'; 
+$view = filter_input(INPUT_GET, 'view') ?: 'list';
 $action = filter_input(INPUT_POST, 'action');
 
-switch ($action) {
-    case 'create':
-        $title = trim((string)(filter_input(INPUT_POST, 'title') ?? ''));
-        $author = trim((string)(filter_input(INPUT_POST, 'author') ?? ''));
-        $price = (float)(filter_input(INPUT_POST, 'price') ?? 0);
-        $format = (int)(filter_input(INPUT_POST, 'format_id') ?? 0);
-        if ($title && $author && $format) {
-            record_create($title, $author, $price, $format);
-            $view = 'created';
-        } else {
-            $view = 'create';
-        }
+if ($action == 'create') {
+    $title = trim((string)(filter_input(INPUT_POST, 'title') ?? ''));
+    $author = trim((string)(filter_input(INPUT_POST, 'artist') ?? ''));
+    $price = (float)(filter_input(INPUT_POST, 'price') ?? 0);
+    $format = (int)(filter_input(INPUT_POST, 'format_id') ?? 0);
+    if ($title && $author && $format) {
+        record_create($title, $author, $price, $format);
+        $view = 'created';
+    } else {
+        $view = 'create';
+        echo ("An Error occured creating your record");
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,6 +28,7 @@ switch ($action) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/styles.css">
 </head>
+
 <body>
     <h1>Record Store</h1>
     <div>
@@ -42,4 +44,5 @@ switch ($action) {
         ?>
     </div>
 </body>
+
 </html>
--- partials/record-form.php
+++ partials/record-form.php
@@ -10,17 +10,17 @@ $format_list = formats_all();
                 <input name="title" class="form-control" required>
             </div>
         <div class="col-md-6">
-                <label class="form-label">Author</label>
+                <label class="form-label">Artist</label>
                 <input name="artist" class="form-control" required>
             </div>
         <div class="col-md-3">
                 <label class="form-label">Price</label>
-                <input name="price" type="number" class="form-control" required>
+                <input name="price" type="number" step="0.01" class="form-control" required>
             </div>
         <div class="col-md-3">
                 <label class="form-label">Format</label>
                 <select name="format_id" class="form-select" required>
-                    <option value="">Select...</option>
+                    <option value="">Format</option>
                     <?php foreach ($format_list as $f): ?>
                         <option value="<?= (int)$f['id'] ?>"><?= htmlspecialchars($f['name']) ?></option>
                     <?php endforeach; ?>
